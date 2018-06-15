90196e0d1bbf2a403788409b07eef12f5a9f3f9a
Himanshu Soni <himanshuson@gmail.com>
2018-06-15 17:33:42 -0500

    added welcome banner with boilerplate info

--- app/pageOne.js
+++ app/pageOne.js
@@ -5,12 +5,57 @@
  */
 
 import React, { Component } from 'react'
-import { Platform, StyleSheet, Text, View } from 'react-native'
+import { Platform, StyleSheet, Text, View, ScrollView } from 'react-native'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
 })
+
+const features = ['wix/react-native-navigation', 'eslint', 'flow', 'jest', 'storybook']
+export const WelcomeBanner = () => (
+  <View
+    style={{
+      flex: 1,
+      alignItems: 'center',
+      margin: 10,
+      padding: 5,
+      borderWidth: 1,
+    }}
+  >
+    <View style={{ height: 40, justifyContent: 'center' }}>
+      <Text
+        style={{
+          fontWeight: 'bold',
+          fontSize: 16,
+          justifyContent: 'center',
+        }}
+      >
+        Welcome to RN_Boilerplate
+      </Text>
+    </View>
+    <ScrollView
+      style={{
+        borderTopWidth: 1,
+        borderColor: 'black',
+        marginTop: 5,
+        width: '100%',
+      }}
+    >
+      {features.map(title => (
+        <Text
+          style={{
+            padding: 5,
+            paddingLeft: 10,
+            width: '100%',
+          }}
+        >
+          {`- ${title}`}
+        </Text>
+      ))}
+    </ScrollView>
+  </View>
+)
 
 type Props = {}
 export default class App extends Component<Props> {
@@ -20,6 +65,7 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <WelcomeBanner />
       </View>
     )
   }
